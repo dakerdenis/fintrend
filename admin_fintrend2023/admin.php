34fe8ef63c4e49f399eb858910dfcb411449af0f
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-06-13 11:23:14 +0400

    added

--- admin_fintrend2023/admin.php
+++ admin_fintrend2023/admin.php
@@ -34,76 +34,17 @@ if (empty($_SESSION["authenticated"]) || $_SESSION["authenticated"] != 'true') {
 
 
         <div class="main__content__wrapper">
-            <div class="main__content__desc__navigation">
-                <div class="main__content__desc">
-                    <p>Список файлов</p>
-                </div>
-
-                <div class="main__content__add_file">
-                    <a href="./new_file.php">Добавить файл</a>
-                </div>
-
-            </div>
-
-
-            <div class="main__content__list">
-                <!-------->
-                <div class="main__content__list_element">
-                    <div class="element__image__desc">
-                        <div class="element__image">
-                            <img src="./style/imgs/file.png" alt="">
-                        </div>
-                        <div class="element__desc">
-                            <p>File_name</p>
-                        </div>
-                    </div>
-
-
-                    <!---->
-                    <div class="element__main__content">
-                        <div class="element__main__content_desc">
-                            <div class="element__main__content_desc_lang">
-                                File Name AZ
-                            </div>
-                            <div class="element__main__content_desc_text">
-                                File description AZ lore File description AZ loreFile description AZ lore File description AZ lore File description AZ lore
-                            </div>
-                        </div>
-                        <div class="element__main__content_desc">
-                        <div class="element__main__content_desc_lang">
-                                File Name EN
-                            </div>
-                            <div class="element__main__content_desc_text">
-                            File description EN lore description EN lore description EN loredescription EN loredescription EN lore
-                            </div>
-                        </div>
-                        <div class="element__main__content_desc">
-                        <div class="element__main__content_desc_lang">
-                                File Name RU
-                            </div>
-                            <div class="element__main__content_desc_text">
-                            File description RU lore description RU loredescription RU loredescription RU loredescription RU loredescription RU lore
-                            </div>
-                        </div>
-                    </div>
-                    <!---->
-                    <div class="element__settings_delete">
-                        <div class="element__settings">
-                            <a href="#">
-                                <img src="./style/imgs/settings.png" alt="">
-                            </a>
-                        </div>
-                        <div class="element__delete">
-                            <a href="#">
-                                <img src="./style/imgs/delete.png" alt="">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
+            <?php if($_GET['page']='data'){
+                include './include/data.php';
+            } else if($_GET[])
+            else{
+                include './include/data.php';
+            }            
+             ?>
         </div>
+
+
+        
 
     </div>
 </body>
